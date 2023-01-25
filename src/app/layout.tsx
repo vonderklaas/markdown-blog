84940eba4f0677b11aa8d52e1f5456a7e2726671
Nick Garbalau <garbalaunick@protonmail.com>
2023-01-25 11:44:06 +0200

    Updated configs, added Tailwind, other preparations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
-import './globals.css'
+import { ReactNode } from 'react';
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='en'>
       <head />
       <body>{children}</body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
